575b3e9b15417c6a5cefcef2931f723a6270da04
Sean Mulholland <seanpmulholland@gmail.com>
2024-07-07 21:18:04 -0700

    tabstop settings

--- init.lua
+++ init.lua
@@ -16,7 +16,11 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
